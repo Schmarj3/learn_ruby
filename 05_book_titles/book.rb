0eec7f34c6c8432eb747f8c0e3f667ca726f513e
Marj E <marj@marjorieetienne.com>
2020-03-30 20:01:39 -0400

    completed the book exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
 # write your code here
+    attr_reader :title
+    
+    # title funciton - reused code from simon says and added of
+    def title=(title)
+        new_title_array = []
+        little_title_words = %w[ the to and if no on a an in over of ]
+
+        title_array = title.split(' ')
+
+        title_array.each_with_index do |word, i| 
+            if i == 0
+                new_title_array << word.capitalize
+            elsif little_title_words.include? word
+                new_title_array << word
+            else
+                new_title_array << word.capitalize
+            end
+        end
+        @title = new_title_array.join(' ')
+    end
 end
